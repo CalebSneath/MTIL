a2cb526b807c7978db6a56b7b4a12f6fdd109b04
Caleb <calebsneathwork@gmail.com>
2022-02-18 12:01:56 -0600

    first commit

---
+++ integration_outputs.cpp
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////////////////////////
+// 
+// Author:           Caleb Sneath, Parker, MD
+// Email:            ansengor@yahoo.com
+// Label:            MTIL
+// Course:           CMPS 5153
+// Semester:         Spring 2022
+// File Description: This file contains the implementation details for the 
+//                   translation output class along with all of 
+//                   its  descendents.
+///////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+#include "integration_outputs.h"
+
+using namespace std;
+
+Integration_Output::setText(string inText)
+{
+    
+}
+
+    // TODO: Any necessary constructor overloads
+void Integration_Input::updateText()
+{
+    // TODO: Implementation
+}
+std::string Integration_Input::getText()
+{
+    // TODO: Implementation
+}
