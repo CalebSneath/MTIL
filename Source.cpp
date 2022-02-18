c55968496d385757f6165823c8e08fe86fc3b0d2
Caleb <calebsneathwork@gmail.com>
2022-02-18 12:08:08 -0600

    Test.

---
+++ Source.cpp
@@ -0,0 +1,47 @@
+///////////////////////////////////////////////////////////////////////////////
+// 
+// Author:           Caleb Sneath, MD, Parker
+// Email:            ansengor@yahoo.com
+// Label:            Machine Translation Integration Layer
+// Course:           CMPS 5153
+// Semester:         Spring 2022
+//
+// Description:
+//       This program is designed to make the translations for a windows
+//       application appear more seamlessly within its intended application
+//       
+// Requirements: 
+//      - Ensure VS Code compiler is setup correctly, uses G++, 
+//        and that all files are in the correct folder, including arial.ttf.
+// Compilation Instructions:
+// - Open command terminal, navigate to this project directory. 
+// - Run each of the following without parentheses:
+//  - (g++ -c main.cpp integration_inputs.cpp integration_outputs.cpp)
+//  - (g++ main.o integration_inputs.o integration_outputs.o -o "MTIL")
+//  - ("MTIL.exe")
+//  - The game should now play and the executable can be renamed.
+//      
+// Files:            
+//      main.cpp    : driver program 
+//      game.h      : a file including the the game class header information
+//      game.cpp    : a file including the game class implementation details
+//      arial.ttf   : font libray
+// Font obtained from: 
+//          https://github.com/zyedidia/SFML.jl/blob/master/assets/arial.ttf
+//      assorted jpg files: image assets
+//      test
+///////////////////////////////////////////////////////////////////////////////
+
+#include <Windows.h>
+#include "integration_inputs.cpp"
+#include "integration_outputs.cpp"
+
+using namespace std;
+
+int main()
+{
+
+
+
+    return 0;
+}
